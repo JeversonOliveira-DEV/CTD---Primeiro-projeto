db0b2ef71a0d30ec7ffa168479bf4c7314e08d2d
DevJevy <jeverson.mano@hotmail.com>
2023-01-08 15:02:36 -0300

    Inicio do projeto

---
+++ .vscode/calculadora.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void main () {
+    int n, m, op;
+    int resultado = n, op, m;
+    scanf("%d",&n );
+    scanf("%c", &op);
+    scanf("%d", &m);
+    printf("%d %c %d", n  );
+} 
